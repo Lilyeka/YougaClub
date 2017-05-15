1ca3b26fdf3552f923dbb894df271587bd34e41b
Лилия Левина <liliyalevina@MacBook-Air-Lilia.local>
2017-05-15 09:50:05 +0500

    Initial Commit

---
+++ YogaClub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YogaClub
+//
+//  Created by Лилия Левина on 15.05.17.
+//  Copyright © 2017 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
